26b2f02781908201b5ca857bc3b4334d537c5663
Arcel Luceno <arcelluceno11@gmail.com>
2022-07-18 11:37:52 +0800

    Initial Commit

--- app/src/main/java/com/capstone/sharity/activity/MainActivity.java
+++ app/src/main/java/com/capstone/sharity/activity/MainActivity.java
@@ -2,22 +2,20 @@ package com.capstone.sharity.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.capstone.sharity.R;
+
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Initialize Toolbar
-    MaterialToolbar materialToolbar;
-
-    //Initialize Navigation
-    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,45 +23,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Toolbar
-        materialToolbar = findViewById(R.id.materialToolbar);
-        materialToolbar.setTitle("");
-        setSupportActionBar(materialToolbar);
-
-        //Navigation
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setSelectedItemId(R.id.shop);
-
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.share:
-                    //getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, new MainShareFragment()).commit();
-                    break;
-                case R.id.shop:
-                    //getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, new MainShopFragment()).commit();
-                    break;
-                case R.id.support:
-                    //getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, new MainSupportFragment()).commit();
-                    break;
-            }
-
-            return true;
-        });
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_content);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        MaterialToolbar materialToolbar = findViewById(R.id.materialToolbar);
+        NavigationUI.setupWithNavController(materialToolbar, navController, appBarConfiguration);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.top_nav_menu,menu);
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.main_toolbar_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_notifications:
-                //getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, new MainNotificationsFragment()).commit();
+
+        switch (item.getItemId()){
+            case R.id.mainToolbarNotificationsActivity:
+                //code here
                 break;
-            case R.id.nav_account:
-                //getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, new MainAccountFragment()).commit();
+            case R.id.mainToolbarAccountActivity:
+                //code here
                 break;
         }
 
--- app/src/main/java/com/capstone/sharity/fragment/MainBottomNavigationShopFragment.java
+++ app/src/main/java/com/capstone/sharity/fragment/MainBottomNavigationShopFragment.java
@@ -12,10 +12,10 @@ import com.capstone.sharity.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link MainNotificationsFragment#newInstance} factory method to
+ * Use the {@link MainBottomNavigationShopFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MainNotificationsFragment extends Fragment {
+public class MainBottomNavigationShopFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class MainNotificationsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public MainNotificationsFragment() {
+    public MainBottomNavigationShopFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class MainNotificationsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NotificationsFragment.
+     * @return A new instance of fragment MainBottomNavigationShopFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static MainNotificationsFragment newInstance(String param1, String param2) {
-        MainNotificationsFragment fragment = new MainNotificationsFragment();
+    public static MainBottomNavigationShopFragment newInstance(String param1, String param2) {
+        MainBottomNavigationShopFragment fragment = new MainBottomNavigationShopFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class MainNotificationsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_notifications, container, false);
+        return inflater.inflate(R.layout.fragment_main_bottom_navigation_shop, container, false);
     }
 }
